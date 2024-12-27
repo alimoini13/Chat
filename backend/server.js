40abd7eba288068db537809ac91d393a5cee561d
alimoini13 <alimoini13@gmail.com>
2024-12-27 10:24:12 +0330

    connecting mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,22 @@ import http from "http";
 import path from "path";
 import { Server } from "socket.io";
 import express from "express";
-
+import cors from 'cors'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config()
+//mongodb
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Mongodb Connected...'))
+  .catch((err) => console.log(err));
+//build
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) => {
@@ -12,7 +25,6 @@ app.get("*", (req, res) => {
 });
 
 const httpServer = http.Server(app);
-
 
 const PORT = process.env.PORT || 4000;
 httpServer.listen(PORT, () => {
