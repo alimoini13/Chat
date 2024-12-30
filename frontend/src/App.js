af2418bb1f7cfd9e53665fb32e1621a988204f0b
alimoini13 <alimoini13@gmail.com>
2024-12-30 08:39:27 +0330

    implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import Dashboard from './Dashboard/Dashboard';
+import LoginPage from './authPages/LoginPage/LoginPage';
+import RegisterPage from './authPages/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <div >
-     <div>its just a test1</div>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+  // <AlertNotification />
   );
 }
 
